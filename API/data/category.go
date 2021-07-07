290bec28123e7ed09db44ae0c36d50fbb7ce78d4
Guilherme <guilherme.coelho@wastevision.com>
2021-07-07 19:53:11 +0100

    put api on separeted folder

---
+++ API/data/category.go
@@ -0,0 +1,80 @@
+package data
+
+import (
+	"fmt"
+
+	"github.com/guilhermecoelho/kakeibo/configurations"
+	"github.com/guilhermecoelho/kakeibo/models"
+)
+
+func GetCategories() (models.Categories, error) {
+
+	category := models.Categories{}
+	result := configurations.DBgorm.Find(&category)
+
+	if result.Error != nil {
+		return category, fmt.Errorf("error on: %v", result.Error)
+	}
+
+	return category, nil
+}
+
+func GetCategoryById(id int) (models.Category, error) {
+
+	category := models.Category{}
+	result := configurations.DBgorm.First(&category, id)
+
+	if result.Error != nil {
+		return category, fmt.Errorf("error on: %v", result.Error)
+	}
+
+	return category, nil
+}
+
+func PutCategory(g *models.Category) (models.Category, error) {
+
+	category := *g
+	result := configurations.DBgorm.Save(&category)
+
+	if result.Error != nil {
+		return category, fmt.Errorf("error on: %v", result.Error)
+	}
+
+	category, err := GetCategoryById(category.Id)
+	if err != nil {
+		return category, fmt.Errorf("error on: %v", result.Error)
+	}
+
+	return category, nil
+
+}
+
+func PostCategory(g *models.Category) (models.Category, error) {
+
+	category := *g
+	category.Id = 0
+	result := configurations.DBgorm.Create(&category)
+
+	if result.Error != nil {
+		return category, fmt.Errorf("error on: %v", result.Error)
+	}
+
+	category, err := GetCategoryById(category.Id)
+	if err != nil {
+		return category, fmt.Errorf("error on: %v", result.Error)
+	}
+
+	return category, nil
+}
+
+func DeleteCategory(g *models.Category) error {
+
+	category := *g
+	result := configurations.DBgorm.Delete(&category)
+
+	if result.Error != nil {
+		return fmt.Errorf("error on: %v", result.Error)
+	}
+
+	return nil
+}
---
+++ API/data/income.go
@@ -0,0 +1,80 @@
+package data
+
+import (
+	"fmt"
+
+	"github.com/guilhermecoelho/kakeibo/configurations"
+	"github.com/guilhermecoelho/kakeibo/models"
+)
+
+func GetIncomes() (models.Incomes, error) {
+
+	income := models.Incomes{}
+	result := configurations.DBgorm.Find(&income)
+
+	if result.Error != nil {
+		return income, fmt.Errorf("error on: %v", result.Error)
+	}
+
+	return income, nil
+}
+
+func GetIncomesById(id int) (models.Income, error) {
+
+	income := models.Income{}
+	result := configurations.DBgorm.First(&income, id)
+
+	if result.Error != nil {
+		return income, fmt.Errorf("error on: %v", result.Error)
+	}
+
+	return income, nil
+}
+
+func PutIncomes(g *models.Income) (models.Income, error) {
+
+	income := *g
+	result := configurations.DBgorm.Save(&income)
+
+	if result.Error != nil {
+		return income, fmt.Errorf("error on: %v", result.Error)
+	}
+
+	income, err := GetIncomesById(income.Id)
+	if err != nil {
+		return income, fmt.Errorf("error on: %v", result.Error)
+	}
+
+	return income, nil
+
+}
+
+func PostIncomes(g *models.Income) (models.Income, error) {
+
+	income := *g
+	income.Id = 0
+	result := configurations.DBgorm.Create(&income)
+
+	if result.Error != nil {
+		return income, fmt.Errorf("error on: %v", result.Error)
+	}
+
+	income, err := GetIncomesById(income.Id)
+	if err != nil {
+		return income, fmt.Errorf("error on: %v", result.Error)
+	}
+
+	return income, nil
+}
+
+func DeleteIncomes(g *models.Income) error {
+
+	income := *g
+	result := configurations.DBgorm.Delete(&income)
+
+	if result.Error != nil {
+		return fmt.Errorf("error on: %v", result.Error)
+	}
+
+	return nil
+}
